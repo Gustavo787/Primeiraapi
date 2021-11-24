7ad5f3d37dce1ee673329caa5dc4e7b775308f80
Gustavo Dos Santos Santana <gustavosantosoficial.az@gmail.com>
2021-11-24 11:29:15 -0300

    Fim da api

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 // Vamos construir um sevidor usano o modulo do Express.
 //Este modo possue fuções para executar e malipular um servidor Node
 //iniciamos e criando uma referencia do Express com a importação do modulo
-const express = require("express")
+const express = require("express");
 
 
 // Vamos importar o modulo Mongoose que fara a interface entre o
 //nodejs e o banco de dados Mongoose
 const mongoose = require("mongoose");
+
+
+// Importar do modulo bcrpt para criptografia de senhas
+const bcrypt = require("bcrypt");
+
+//Jsonwebtiken é um hash que garante  a seção segura em página ou grupos de páginas
+//permitindo ou não o acesso aos conteúdos desta página. Ele é gerando a partir de alguns
+//elementos, tais: dados  que importam ao token(payload), chave secreta, tempo de
+//expiração e método de criptografia.
+const jwt = require("jsonwebtoken");
+
+const cfn = require("./config");
 
 
 const url = "mongodb+srv://gustavo:Gustavo18@clustercliente.zrloi.mongodb.net/primeiraapi?retryWrites=true&w=majority"
@@ -22,6 +34,20 @@ const tabela = mongoose.Schema({
     usuario:{type:String, required:true,unique:true},
     senha:{type:String, required:true,unique:true},
 });
+
+// Aplicação da criptografia do bcrypt a tabela de cadastro
+// de clientes será um passo antes do salvamento
+// vamos usar o comando pre
+tabela.pre("save",function(next){
+    let cliente = this;
+    if(!cliente.isModified(`senha`)) return next()
+    bcrypt.hash(cliente.senha,10,(erro,rs)=>{
+        if(erro) return console.log(`erro ao gerar senha ->${erro}`);
+        cliente.senha = rs
+        return next();
+    });
+});
+
 
 // execução da tabela
 const Cliente = mongoose.model("tbcliente",tabela);
@@ -62,35 +88,91 @@ app.get("/api/cliente/",(req,res)=>{
     );
 });
 
+app.get("/api/cliente/:id",(req,res)=>{
+    Cliente.findById(req.params.id,(erro,dados)=>{
+        if (erro){
+            return res.status(400).send({output:`Erro ao temtar ler os clientes -> ${erro}`});
+        }
+        res.status(201).send({oninput:dados})
+    }
+        
+    );
+});
+
+
+
 app.post("/api/cliente/cadastro",(req,res)=>{
     
     const cliente = new Cliente(req.body);
     cliente.save().then(()=>{
-         res.status(201).send({oninput:`Cliente cadastrado`})
+        const gerado = criarToken(req.body.usuario, req.body.nome);
+        res.status(201).send({oninput:`Cliente cadastrado`, token:gerado});
     })
     .catch((erro)=>res.status(400).send({oninput:`Erro ao tentar cadastrar o cliente -> ${erro}`}))
 
 });
 
-app.put("/api/cliente/atualizar/:id",(req,res)=>{
+
+
+app.post("/api/cliente/login",(req,res)=>{
+    const us = req.body.usuario;
+    const sh = req.body.senha;
+    Cliente.findOne({usuario:us},(erro,dados)=>{
+        if(erro){
+           return res.status(400).send({output:`Usuario não localizado ->${erro}`})
+        }
+        
+        bcrypt.compare(sh,dados.senha,(erro,igual)=>{
+            if(erro) return res.status(400).send({output:`Erro ao tentar logar->${erro}`});
+            if(!igual) return res.status(400).send({output:`Erro ao tentar logar senha->${erro}`});
+            const gerado = criarToken(dados.usuario,dados.nome);
+            res.status(200).send({output:`Logado`,palyload:dados, token:gerado});
+        })
+
+    })
+});
+
+app.put("/api/cliente/atualizar/:id", verifica, (req,res)=>{
     Cliente.findByIdAndUpdate(req.params.id,req.body,(erro,dados)=>{
       if(erro){
         return res.status(400).send({output:`Erro ao tentar atualizar ->${erro}`});
     }
     res.status(200).send({output:`Dados atualizados`}); 
-})
+});
 
 });
 
-app.delete("/api/cliente/apagar/:id",(req,res)=>{
+app.delete("/api/cliente/apagar/:id", verifica, (req,res)=>{
     Cliente.findByIdAndDelete(req.params.id,(erro,dados)=>{
       if(erro){
           return res.status(400).send({output:`Erro ao tentar apagar o cliente->${erro}`})
-      }
+        }
       res.status(204).send({});
     });
 
 });
+
+// Gerar token
+const criarToken=(usuario, nome)=>{
+    return jwt.sign({usuario:usuario, nome:nome}, cfn.jwt_key,{expiresIn:cfn.jwt_expires});
+    
+};
+
+
+// Validção do token
+function verifica(req,res,next){
+    const token_gerado = req.headers.token;
+    if(!token_gerado)
+        return res.status(401).send({output:"Token não encontrado"});
+
+    jwt.verify(token_gerado, cfn.jwt_key,(erro,dados)=>{
+        if(erro){
+            return res.status(401).send({output:"Token inválido"});
+        }
+         res.status(200).send({output:`Autorizado`,palyload:`Olá ${dados.nome}`});
+         next();
+    });
+};
 
 app.listen(3000,()=>console.log("Servidor online em http://localhost:3000"));
 
